baed39b85aa238df366cb3521acc0619ee710d8e
Roberto Molinari <roberto.molinari@protonmail.com>
2026-03-30 17:06:02 -0400

    Some more small changes in attempt to personalize the setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Roberto Molinari's Blog";
+export const SITE_TITLE = "🍕";
 export const SITE_DESCRIPTION = "Simple blog site to share my thoughts and ideas";
